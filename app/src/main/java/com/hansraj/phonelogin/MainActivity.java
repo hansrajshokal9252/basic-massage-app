49462fde86b46bb9889893eefbc4511e79ef582c
hansrajshokal9252 / <8386913653Hr@>
2021-07-11 15:41:53 +0530

    show_all_user_ondeshboard

--- app/src/main/java/com/hansraj/phonelogin/MainActivity.java
+++ app/src/main/java/com/hansraj/phonelogin/MainActivity.java
@@ -1,14 +1,120 @@
 package com.hansraj.phonelogin;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.net.Uri;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.Toast;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
+import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.UploadTask;
+import com.hansraj.phonelogin.databinding.ActivityMainBinding;
+
+import java.util.ArrayList;
+import java.util.Date;
+import java.util.HashMap;
 
 public class MainActivity extends AppCompatActivity {
+
+    ActivityMainBinding binding;
+    FirebaseDatabase database;
+    ArrayList<User> users;
+    UsersAdapter usersAdapter;
+    //ProgressDialog dialog;
+
+    User user;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        /*dialog = new ProgressDialog(this);
+        dialog.setMessage("Uploading Image...");
+        dialog.setCancelable(false);*/
+
+        database = FirebaseDatabase.getInstance();
+        users = new ArrayList<>();
+
+        /*database.getReference().child("users").child(FirebaseAuth.getInstance().getUid())
+                .addValueEventListener(new ValueEventListener() {
+                    @Override
+                    public void onDataChange(@NonNull DataSnapshot snapshot) {
+                        user = snapshot.getValue(User.class);
+                    }
+
+                    @Override
+                    public void onCancelled(@NonNull DatabaseError error) {
+
+                    }
+                });*/
+
+
+        usersAdapter = new UsersAdapter(this, users);
+//        binding.recyclerView.setLayoutManager(new LinearLayoutManager(this));
+
+        binding.recyclerView.setAdapter(usersAdapter);
+
+        database.getReference().child("users").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                users.clear();
+                for (DataSnapshot snapshot1 : snapshot.getChildren()) {
+                    User user = snapshot1.getValue(User.class);
+                    if (!user.getUid().equals(FirebaseAuth.getInstance().getUid()))
+                        users.add(user);
+                }
+                usersAdapter.notifyDataSetChanged();
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
+
+
+
+
+
+    }
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        switch (item.getItemId()) {
+            case R.id.group:
+                Toast.makeText(this, "group clicked.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.search:
+                Toast.makeText(this, "Search clicked.", Toast.LENGTH_SHORT).show();
+                break;
+            case R.id.settings:
+                Toast.makeText(this, "Settings Clicked.", Toast.LENGTH_SHORT).show();
+                break;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
+        getMenuInflater().inflate(R.menu.topmenu, menu);
+        return super.onCreateOptionsMenu(menu);
     }
 }
